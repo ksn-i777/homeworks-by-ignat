f67edd884bdb248f7fa896eac0a7e9f38834ecf9
ksn-i777 <ksn_i777@mail.ru>
2022-12-07 23:14:38 +0300

    wednesday-2-hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -7,25 +7,29 @@ import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
-* 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
-* 4 - сделать стили в соответствии с дизайном
+* 1+ - в файле loadingReducer.ts дописать типы и логику
+* 2+ - получить isLoading из редакса
+* 3+ - дописать функцию setLoading
+* 4+ - сделать стили в соответствии с дизайном
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    //+ useSelector, useDispatch //+ пишет студент
+    const isLoading = useSelector<AppStoreType, boolean>(st => st.loading.isLoading)
+    const dispatch = useDispatch()
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
-
-        // setTimeout
+    const setLoading = () => { //+ пишет студент //+ показать крутилку на 1,5 секунд
+        //+ dispatch
+        dispatch(loadingAC(true))
+        //+ setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
+        <div id={'hw10'} className={s2.wrapper}>
+            <div className={s2.hwTitle}>Homework №10</div>
 
             <div className={s2.hw}>
                 {isLoading ? (
@@ -33,12 +37,14 @@ const HW10 = () => {
                         <Loader/>
                     </div>
                 ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
+                    <div style={{marginTop: '40px'}}>
+                        <SuperButton
+                            id={'hw10-button-start-loading'}
+                            onClick={setLoading}
+                        >
+                            Set loading...
+                        </SuperButton>
+                    </div>
                 )}
             </div>
         </div>
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,3 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <img className={s.loader} src='https://thechurchofthecross.net/wp-content/plugins/3d-flip-book/assets/images/light-loader.gif' alt='loader'/>
